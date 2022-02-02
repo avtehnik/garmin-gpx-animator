5b5bbd73e9df52a4719e1ccceadbf1044246dd5e
Vitaliy Pitvalo <av.tehnik@gmail.com>
2022-02-03 01:58:10 +0200

    add info

--- helpers.js
+++ helpers.js
@@ -195,3 +195,14 @@ class Line extends maptalks.BaseObject {
         this.getObject3d().position.z = z;
     }
 }
+
+
+function dateToText(date){
+    const month = String(date.getMonth()).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    return day + '.' + month + '.' + year+ ' ' +  hours + ":" + minutes + ":" + seconds;
+}
